4f3866d91d05b75b12f704965010d9fe989ae128
laszlogulyas <laszlogulyas.jr@gmail.com>
2021-10-02 14:48:25 +0200

    Lesson 14-21

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<h3>hello from the servers component</h3>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
